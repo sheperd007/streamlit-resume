4459348d7d62fd2327eca558eda514f1a52ee2bd
Hamid <hamidjahani999@gmail.com>
2023-11-04 14:31:40 +0330

    add my resume

---
+++ constant.py
@@ -0,0 +1,26 @@
+info = {
+   "Pronoun": "his",
+   "Name": "Hamid",
+   "Full_Name": "Hamid Jahani",
+   "Intro": "A Tech enthusiast and Data Scientist at RoomVU",
+   "About":"Hey there, I'm Hamid! I'm passionate about sharing my technical and data knowledge. My expertise covers a wide range of topics, from AI and Statistics to Data Engineering, Cloud, and more. You can explore the wealth of courses and resources I've created at bigdataworld.ir.",
+   "City":"Tehran, Iran",
+   "Photo":"""<a href=\"https://www.linkedin.com/in/hamid-jahani/\"><img src=\"https://pps.whatsapp.net/v/t61.24694-24/290239982_719387295846842_8596890817938210029_n.jpg?ccb=11-4&oh=01_AdR0NjZjOETTIR4huYlxHvyhSc-EZ_OuN1oU20xUNEauSA&oe=654FB918&_nc_sid=e6ed6c&_nc_cat=106" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>""",
+   "Email": "hamidjahani999@gmail.com"
+}
+
+embed_rss= {
+    'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+        data-rss="https://www.linkedin.com/in/hamid-jahani/"
+        data-maxcols="3" 
+        data-layout="grid"
+        data-poststyle="inline" 
+        data-readmore="Read the rest" 
+        data-buttonclass="btn btn-primary" 
+        data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+}
+
+endorsements = {
+    "img1": "https://imageupload.io/ib/Y6XSk6hwoWhGih3_1699093979.png",
+    "img2": "https://imageupload.io/ib/NbQgS8iSmfMcn9D_1699093979.png"
+}
